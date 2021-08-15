922a8ed3e3d2d6b230c9717fd94a3fe680586d3e
Ryan Cather <ryan.cather@ed.act.edu.au>
2021-08-15 22:19:19 +1000

    Visual aspects for the edit page.

---
+++ edit.php
@@ -0,0 +1,48 @@
+<?php include "template.php";
+/**
+ *  This is the user's profile page.
+ * It shows the Users details including picture, and a link to edit the details.
+ *
+ * @var SQLite3 $conn
+ */
+?>
+<title>Edit your Profile</title>
+
+<h1 class='text-primary'>Edit Your Profile</h1>
+
+<?php
+if (isset($_SESSION["username"])) {
+    $userName = $_SESSION["username"];
+    $userId = $_SESSION["user_id"];
+
+    $query = $conn->query("SELECT * FROM user WHERE username='$userName'");
+    $userData = $query->fetchArray();
+    $userName = $userData[1];
+    $password = $userData[2];
+    $name = $userData[3];
+    $profilePic = $userData[4];
+    $accessLevel = $userData[5];
+} else {
+    header("Location:index.php");
+}
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Username : <?php echo $userName; ?></h3>
+            <p> Name : <?php echo $name ?> </p>
+            <p> Access Level : <?php echo $accessLevel ?> </p>
+            <p>Profile Picture:</p>
+            <?php echo "<img src='images/profilePic/" . $profilePic . "' width='100' height='100'>" ?>
+        </div>
+        <div class="col-md-6">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+                <p>Name: <input type="text" name="name" value="<?php echo $name ?>"></p>
+                <p>Access Level: <input type="text" name="accessLevel" value="<?php echo $accessLevel ?>"></p>
+                <p>Profile Picture: <input type="file" name ="file"></p>
+                <input type="submit" name="formSubmit" value="Submit">
+            </form>
+        </div>
+    </div>
+</div>
