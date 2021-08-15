ec0eaac661898f8f453362e0c92958eaef367f44
Ryan Cather <ryan.cather@ed.act.edu.au>
2021-08-16 00:02:06 +1000

    Creating the messaging table

--- config.php
+++ config.php
@@ -50,12 +50,14 @@ function addProduct($productName, $category, $quantity, $price, $image, $code)
 
 $query = file_get_contents("sql/create-user.sql");
 createTable($query, "User");
-$query = file_get_contents("sql/create-messaging.sql");
-createTable($query, "Messaging");
 $query = file_get_contents("sql/create-orderDetails.sql");
 createTable($query, "Order Details");
 $query = file_get_contents("sql/create-products.sql");
 createTable($query, "Products");
+$query = file_get_contents("sql/create-messaging.sql");
+createTable($query, "Messaging");
+
+
 
 $query = $conn->query("SELECT COUNT(*) as count FROM user");
 $rowCount = $query->fetchArray();
