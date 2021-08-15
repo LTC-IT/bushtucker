8e877447bde9565fe9c35ea59a3cc72cac5f998e
Ryan Cather <ryan.cather@ed.act.edu.au>
2021-08-15 21:06:11 +1000

    Profile page loading and displaying raw data.

--- profile.php
+++ profile.php
@@ -1,4 +1,32 @@
-<?php include "template.php"; ?>
+<?php include "template.php";
+/**
+ *  This is the user's profile page.
+ * It shows the Users details including picture, and a link to edit the details.
+ *
+ * @var SQLite3 $conn
+ */
+?>
 <title>User Profile</title>
 
-<h1 class='text-primary'>Profile Page</h1>
+<h1 class='text-primary'>Your Profile</h1>
+
+<?php
+$userName = $_SESSION["username"];
+$userId = $_SESSION["user_id"];
+
+$query = $conn->query("SELECT * FROM user WHERE username='$userName'");
+$userData = $query->fetchArray();
+$userName = $userData[1];
+$password = $userData[2];
+$name = $userData[3];
+$profilePic = $userData[4];
+$accessLevel = $userData[5];
+
+echo $userName."<p>";
+echo $password ."<p>";
+echo $name."<p>";
+echo $profilePic."<p>";
+echo $accessLevel."<p>";
+
+?>
+
